652454d2f02f28c6f71887c16a9cf6b38c891761
Simplystina <62130713+Simplystina@users.noreply.github.com>
2023-08-22 18:49:52 +0100

    added authentication to questions routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 var express = require("express");
 const controller = require("../controller/questions");
 var router = express.Router();
-
+const protection = require("../middleware/auth").authentication
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).send('Welcome to Question Generator API');
 });
 
-router.post("/generate", controller.generate);
-router.post("/save", controller.save);
-router.get("/history", controller.history);
+router.post("/generate", protection, controller.generate);
+router.post("/save", protection, controller.save);
+router.get("/history", protection, controller.getHistory);
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 var express = require('express');
-var router = express.Router();
 const userController = require("../controller/users")
+var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 router.post('/signup', userController.signup)
 router.post('/login', userController.login)
+
 module.exports = router;
