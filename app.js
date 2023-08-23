efbd50fe58c9188e5ab79f2750ec83bb53140219
Simplystina <62130713+Simplystina@users.noreply.github.com>
2023-08-23 18:30:33 +0100

    Co-authored-by: Ogoh Cyril <romzy234@users.noreply.github.com>

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/',(req,res)=>{res.send("Welcome to Question Generator")})
 app.use('/question', questionsRouter);
 app.use('/users', usersRouter);
 
